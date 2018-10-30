8e5a930adae496f705bdc6b782f00fd1ad629c71
R Mike Evans <30809235+RMLEvans@users.noreply.github.com>
2018-10-30 11:34:34 +0000

    Added an example batch script for launching multiple simulations

---
+++ exampleBatchScript.bat
@@ -0,0 +1,17 @@
+.\BD_Cumulative.exe 6 0.1 0.2 128 false 10000.0 > .\BDf_CumulatBR0.2mu0.1.csv
+.\BD_Cumulative.exe 6 0.05 0.8 128 false 10000.0 > .\BDf_CumulatBR0.8mu0.05.csv
+.\BD_Cumulative.exe 6 0.01 20 128 false 10000.0 > .\BDf_CumulatBR20mu0.01.csv
+.\BD_Cumulative.exe 6 0.005 80 128 false 10000.0 > .\BDf_CumulatBR80mu0.005.csv
+.\BD_Cumulative.exe 6 0.0046188 4 128 false 10000.0 > .\BDf_CumulatBR4mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 6 128 false 10000.0 > .\BDf_CumulatBR6mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 8 128 false 10000.0 > .\BDf_CumulatBR8mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 10 128 false 10000.0 > .\BDf_CumulatBR10mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 14 128 false 10000.0 > .\BDf_CumulatBR14mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 20 128 false 10000.0 > .\BDf_CumulatBR20mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 31 128 false 10000.0 > .\BDf_CumulatBR31mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 40 128 false 10000.0 > .\BDf_CumulatBR40mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 56 128 false 10000.0 > .\BDf_CumulatBR56mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 80 128 false 10000.0 > .\BDf_CumulatBR80mu0.0046188.csv
+.\BD_Cumulative.exe 6 0.0046188 100 128 false 10000.0 > .\BDf_CumulatBR100mu0.0046188.csv
+
+
